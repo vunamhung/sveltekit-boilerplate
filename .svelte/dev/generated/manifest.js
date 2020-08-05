c3eb2cd8a5fe907a7acb1fd27313f44d55fa8135
Hung Vu <vunamhung@gmail.com>
2020-08-05 18:21:13 +0700

    :tada: init

---
+++ .svelte/dev/generated/manifest.js
@@ -0,0 +1,15 @@
+import * as layout from "../components/layout.svelte";
+
+const components = [
+	() => import("../../../src/routes/index.svelte")
+];
+
+const d = decodeURIComponent;
+const empty = () => ({});
+
+export const routes = [
+	// src/routes/index.svelte
+[/^\/$/, [components[0]]]
+];
+
+export { layout };
